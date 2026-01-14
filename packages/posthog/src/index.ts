f71c910eab3ab0067a69736c9213604ddf2b3839
Cooper Corbett <coopercorbett@gmail.com>
2026-01-14 17:13:34 +1100

    ♻️ refactor(posthog): export all services from package index
    - Add exports for Dashboards, FeatureFlags, Insights, Cohorts
    - Add exports for Events, Persons, Surveys, Actions
    - Add exports for Annotations and Experiments services

--- packages/posthog/src/index.ts
+++ packages/posthog/src/index.ts
@@ -28,3 +28,13 @@ export * as Traits from "./traits.js";
 
 // Services
 export * as Me from "./services/me.js";
+export * as Dashboards from "./services/dashboards.js";
+export * as FeatureFlags from "./services/feature-flags.js";
+export * as Insights from "./services/insights.js";
+export * as Cohorts from "./services/cohorts.js";
+export * as Events from "./services/events.js";
+export * as Persons from "./services/persons.js";
+export * as Surveys from "./services/surveys.js";
+export * as Actions from "./services/actions.js";
+export * as Annotations from "./services/annotations.js";
+export * as Experiments from "./services/experiments.js";
